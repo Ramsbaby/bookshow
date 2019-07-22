875ee63fa17ae2f1d86a406e00bec354b532d302
JUNGWOOLEE <ms6698@naver.com>
2019-07-23 01:06:56 +0900

    third commit

--- src/main/java/com/jwlee/bookshow/BookshowApplication.java
+++ src/main/java/com/jwlee/bookshow/BookshowApplication.java
@@ -1,6 +1,7 @@
 package com.jwlee.bookshow;
 
-import com.jwlee.bookshow.webapp.db.login.model.LoginEntity;
+import com.jwlee.bookshow.webapp.common.SessionManager;
+import com.jwlee.bookshow.webapp.db.login.model.Login;
 import com.jwlee.bookshow.webapp.db.login.repository.LoginRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,6 +11,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 /**
 * DemoApplication
@@ -24,16 +27,48 @@ public class BookshowApplication implements CommandLineRunner {
 	@Autowired
 	private LoginRepository loginRepository;
 
-	@RequestMapping("/")
-	String loginJsp() throws Exception{
-        return "login";
+	//********************   로그인 페이지 관련   ********************
+	@RequestMapping(value = {"/","/login","/login.do"})
+	String loginJsp(HttpSession session) throws Exception{
+		session.invalidate();
+		return "login/login";
 	}
 
 	@RequestMapping(value="/login/popup/pAccountAdd.do") public String pAccountAdd(Model model) {return "login/popup/pAccountAdd";}
+	//********************   로그인 페이지 관련   ********************
+
+
+
+	//********************   메인 페이지 관련   ********************
+	@RequestMapping("/main/bookSearch.do")
+	String bookSearch() throws Exception{
+		// check session
+		if(SessionManager.getUser() == null) {
+			return "redirect:/login.do";
+		}
+        return "main/bookSearch";
+	}
+	@RequestMapping("/main/myHistory.do")
+	String myHistory() throws Exception{
+		if(SessionManager.getUser() == null) {
+			return "redirect:/login.do";
+		}
+        return "main/myHistory";
+	}
+	@RequestMapping("/main/bestSeller.do")
+	String bestSeller() throws Exception{
+		if(SessionManager.getUser() == null) {
+			return "redirect:/login.do";
+		}
+        return "main/bestSeller";
+	}
+	//********************   메인 페이지 관련   ********************
+
+
 
 	@Override
 	public void run(String... args) throws Exception{
-		loginRepository.save(new LoginEntity("test","test"));
+//		loginRepository.save("test".name("test").cellTel("test").password("test").build());
 	}
 
 	public static void main(String[] args) {
